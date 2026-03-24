4d4090ef12736eef3edb3d7d148270203fc0ff7f
MetehanBatal <47251500+MetehanBatal@users.noreply.github.com>
2026-03-24 13:35:33 +0300

    Added new order page & attribute change on /continue

--- src/routes/order/+page.svelte
+++ src/routes/order/+page.svelte
@@ -55,4 +55,4 @@
     <script src="https://js.stripe.com/basil/stripe.js"></script>
 </svelte:head>
 
-<savvy id="flow_5e8j438g43g77564i11j7f02j3" lp="landing_page" intake="telegra"></savvy>
+<savvy id="flow_5e8j438g43g77564i11j7f02j3" intake="order"></savvy>
